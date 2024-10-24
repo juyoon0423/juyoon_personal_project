1db5ae7ca1e0e3f4857899b1cb7195451c381469
juyoon <juyoon0423@daum.net>
2024-10-24 17:59:46 +0900

    commit Test

--- src/main/java/juyoon/restfuljourney/service/MemberService.java
+++ src/main/java/juyoon/restfuljourney/service/MemberService.java
@@ -26,7 +26,7 @@ public class MemberService {
     private void validateDuplicateMember(Member member) {
         Optional<Member> findMembers = memberRepository.findByEmail(member.getEmail());
         if (findMembers.isPresent()) {
-            throw new IllegalStateException("이미 존재하는 회원입니다");
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
         }
     }
 
